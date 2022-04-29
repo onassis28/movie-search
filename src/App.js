5df6ade723033e9eb8fbcfdef85bb27dc1b2789c
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-04-30 00:03:40 +0200

    rendering made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Card from './components/movie card/card'
 
 const App = () => {
   return (
     <>  
     
    < Header />
-    
+    < Card/>
     
     
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './header.css'
 import {BsSearch} from 'react-icons/bs'
+import { useState } from 'react'
 
 const Header = () => {
+    const [search, setSearch]=useState('Avengers')
+    const handleChange =(e)=>{setSearch(e.target.value)
+    }
   return (
     <div className='header__container'><h1>Movie Search </h1>
-        <div className='search__container'> <i><BsSearch className='icon__header'/></i><input className='input__header' type="text" placeholder='Search' /></div></div>
+        <div className='search__container'> <i><BsSearch className='icon__header'/></i><input onChange={handleChange} className='input__header' type="text" placeholder='Search' /></div></div>
   )
 }
 
