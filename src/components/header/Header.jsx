4179e42f9791245e1f2d99ff26190d6794ee76b5
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-05-04 00:19:05 +0200

    adding search functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import './header.css'
 import {BsSearch} from 'react-icons/bs'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Card from '../movie card/card'
 
-const Header = () => {
-    const [search, setSearch]=useState('Avengers')
-    const handleChange =(e)=>{setSearch(e.target.value)
-    }
+    
+ 
+
+const Header = (props) => {
+    const [search, setSearch]=useState('Home Alone')
+    const handleChange =(e)=>{setSearch(e.target.value)}
+    const [data, setData]= useState([])
+    useEffect( ()=>{
+        fetch(`http://www.omdbapi.com/?s=${search}&apikey=d31e74bc`)
+.then(response=> response.json())
+.then(data=> {setData(data.Search)
+
+
+})
+
+
+
+
+
+    },[search])
+
+    const dataRender =data.map(item=>{
+        return <Card
+        key={item.imbdbID}
+        year ={item.Year}
+        poster={item.Poster}
+        title={item.Title}
+        />
+    })
+    
+
+ 
+
+   
+    
   return (
+      <>
     <div className='header__container'><h1>Movie Search </h1>
         <div className='search__container'> <i><BsSearch className='icon__header'/></i><input onChange={handleChange} className='input__header' type="text" placeholder='Search' /></div></div>
+       <section className='dataRender'>{dataRender}</section> 
+        
+        </>
   )
 }
 
 export default Header
+
--- src/components/movie card/card.jsx
+++ src/components/movie card/card.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './card.css'
-import { useState } from 'react'
 
-const card = () => {
+
+const card = (props)=>{
     
   return (
     <div className='card'>
         <div className='card__container'>
         <div className="card__items">
-        <div><img src="#" alt="movie poster" /></div>
-        <h2>Movie Tile</h2>
-        <p>Movie year</p>
+        <div><img src={props.poster} alt="movie poster" /></div>
+        <p>Movie Title: {props.title}</p>
+        <p> Year: {props.year}</p>
 
         
         
@@ -24,6 +24,4 @@ const card = () => {
 
 export default card
 
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d31e74bc')
-.then(response=> response.json())
-.then(data=>console.log(data.Actors))
+
